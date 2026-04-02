be18e3059c9b92061c1f7f68b3ba511f7ad314ae
Mustafa-Ki-O <robnsonmark11@gmail.com>
2026-04-02 17:27:31 +0300

    update Apply req to subapase 2

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
+console.log("Checking Supabase URL:", process.env.SUPABASE_URL);
+console.log("Checking Supabase Service Key:", process.env.SUPABASE_SERVICE_KEY);
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY 
